aae9c2ad264677e5900af1a88e668d475b4aaf90
AnanthMSIS <113513284+AnanthMSIS@users.noreply.github.com>
2022-12-02 16:39:58 +0530

    Add files via upload

---
+++ Assignment2/P4/Project/Objects/sine.d
@@ -0,0 +1,4 @@
+.\objects\sine.o: ..\Code\sine.c
+.\objects\sine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\sine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\sine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
---
+++ Assignment2/P5/Project/Objects/main.d
@@ -0,0 +1,4 @@
+.\objects\main.o: ..\Code\main.c
+.\objects\main.o: ..\Code\head.h
+.\objects\main.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
