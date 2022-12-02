547a034882125b1a301d54b7f3c45f16e1f75751
AnanthMSIS <113513284+AnanthMSIS@users.noreply.github.com>
2022-12-02 16:39:02 +0530

    Add files via upload

---
+++ Assignment2/P2/Project/Objects/main.d
@@ -0,0 +1,3 @@
+.\objects\main.o: ..\Code\main.c
+.\objects\main.o: ..\Code\head.h
+.\objects\main.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
---
+++ Assignment2/P3/Code/main.c
@@ -0,0 +1,16 @@
+#include"head.h"
+int main()
+{ 
+	pll_init();
+	uart_int();
+	int num=0;
+	while(1)
+	{
+		num=recv_num();
+		if(num==0) 
+			continue;
+		else
+			armstrong(num);
+	}
+	return 0;
+}
